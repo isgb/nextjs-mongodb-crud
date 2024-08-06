e663f722b94b8d5e954b9ef79fa8808ac141fa79
oscar <oscar.vasquez@zentro.com.mx>
2024-08-06 16:05:36 -0600

    debug

---
+++ src/app/api/tasks/[id]/route.js
@@ -0,0 +1,20 @@
+const { NextResponse } = require("next/server");
+
+export function GET(request, {params}){
+    console.log(params);
+    return NextResponse.json({
+        message: "obteniendo tarea..."
+    })
+}
+
+export function DELETE(request, {params}){
+    return NextResponse.json({
+        message: `eliminando tarea ${params.id}`
+    })
+}
+
+export function PUT(request, {params}){
+    return NextResponse.json({
+        message: `actualizando tarea ${params.id}`
+    })
+}
---
+++ src/app/api/tasks/route.js
@@ -0,0 +1,13 @@
+const { NextResponse } = require("next/server");
+
+export function GET(){
+    return NextResponse.json({
+        message: "obteniendo tareas...",
+    });
+}
+
+export function POST(){
+    return NextResponse.json({
+        message: "creando tarea..."
+    })
+}
