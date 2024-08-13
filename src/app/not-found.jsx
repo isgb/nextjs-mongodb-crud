bb78b6efa9d7578f3f46742cbfde42448562e5de
isaias_pruebas <isgb_codigo@hotmail.com>
2024-08-12 22:03:28 -0600

    debug

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+
+const NotFound = () => {
+  return (
+    <section className="h-[calc(100vh-7rem)] flex items-center justify-center flex-col">
+        <h1 className="text-7xl font-bold blo">404</h1>
+        <p>
+            Page Not Found
+        </p>
+    </section>
+  )
+}
+
+export default NotFound
--- src/componentes/TaskCard.jsx
+++ src/componentes/TaskCard.jsx
@@ -5,8 +5,14 @@ const TaskCard = ({ task }) => {
     <Link href={`/tasks/${task._id}`}>
       <div className="bg-gray-800 text-white rounded-md 
         hover:cursor-pointer hover:bg-gray-700">
-        <h1>{task.title}</h1>
-        <p>{task.description}</p>
+        <h3 className="text-2xl font-bold">{task.title}</h3>
+        <p className="text-slate-300">{task.description}</p>
+        <p className="text-slate-400">
+          <span className="mr-1">
+            Created At:
+          </span>
+          {new Date(task.createdAt).toLocaleDateString()}
+        </p>
       </div>
     </Link>
   )
