5ffdcb64688d8b19dfbe03048aef455add3381d6
isaias_pruebas <isgb_codigo@hotmail.com>
2024-08-12 22:12:53 -0600

    debug

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,8 +2,8 @@
 const NotFound = () => {
   return (
     <section className="h-[calc(100vh-7rem)] flex items-center justify-center flex-col">
-        <h1 className="text-7xl font-bold blo">404</h1>
-        <p>
+        <h1 className="text-7xl font-bold blod block">404</h1>
+        <p className="text-slate-300 my-5 text-3xl">
             Page Not Found
         </p>
     </section>
